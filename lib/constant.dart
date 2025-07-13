ebc5edfbec5a4fdb4f989e0c57445b1f61911dff
nayra92 <naira.hashem@gmail.com>
2025-07-14 00:39:29 +0300

    new file:   images/apple.jpg 	new file:   images/banana.jpg 	modified:   lib/constant.dart 	modified:   lib/home_screen.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 Color primaryColor = const Color(0xff53B175);
+Color greyColor = const Color(0xffE2E2E2);
+Color greyTextColor = const Color(0xff7C7C7C);
 
-Widget app_ele_button(
-  String text,
-  Function() fun
-){
+Widget app_ele_button(String text, Function() fun) {
   return Container(
     width: 353,
     height: 67,
@@ -18,19 +16,101 @@ Widget app_ele_button(
           borderRadius: BorderRadius.circular(16.0),
         ),
       ),
-      child: Text(
-        text,
-        style: app_text_style()
-      ),
+      child: Text(text, style: app_text_style()),
     ),
   );
 }
 
-TextStyle app_text_style(){
+TextStyle app_text_style() {
   return TextStyle(
-    color: Colors.white,
-        fontSize: 18.0,
-        fontWeight: FontWeight.w600,
-      fontFamily: "Gilroy"
+      color: Colors.white,
+      fontSize: 18.0,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Gilroy");
+}
+
+Widget app_items_card(
+    String image_url, String item_name, String item_count, String item_price) {
+  return Container(
+    width: 180,
+    padding: const EdgeInsets.all(5),
+    margin: const EdgeInsets.all(5),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(16),
+      border: Border.all(
+        color: greyColor,
+        width: 1,
+      ),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  image: DecorationImage(
+                      image: AssetImage(image_url), fit: BoxFit.cover)),
+            )),
+        SizedBox(
+          height: 20,
+        ),
+        Text(item_name,
+            style: app_text_style().copyWith(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
+        Text(item_count,
+            style: app_text_style().copyWith(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: greyTextColor,
+            )),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("\$$item_price",
+                  style: app_text_style().copyWith(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black)),
+              IconButton(
+                  style: IconButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                  ),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ))
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+// Carousel home page view
+Container myCarouselContainer(String image_url) {
+  return Container(
+    height: 115,
+    decoration: BoxDecoration(
+        color: greyColor,
+        image:
+            DecorationImage(image: AssetImage(image_url), fit: BoxFit.cover)),
   );
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'constant.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -10,6 +12,142 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("images/single_logo.png", color: Colors.orange,),
+              Text("Aden, Yemen",
+              style: app_text_style().copyWith(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: greyTextColor
+              ),),
+
+              // Carousel View items
+              SizedBox(
+                height: 150,
+                child: CarouselView(
+                  itemExtent: 400, 
+                  children: [
+                     myCarouselContainer(
+                  "images/banana.jpg"
+                ),
+                myCarouselContainer(
+                  "images/apple.jpg"
+                ),
+                myCarouselContainer(
+                  "images/banana.jpg"
+                ),
+                  ]),
+              ),
+             
+          
+              // Exclusive Offers items
+              SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text("Exclusive Offer",
+                    style: app_text_style().copyWith(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                    ),),
+                    Text("See all",
+                    style: app_text_style().copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: primaryColor
+                    ),)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 280,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    app_items_card(
+                      "images/banana.jpg",
+                      "Organic Bananas",
+                      "7pcs, Pricing",
+                      "4.99"
+                    ),
+                    app_items_card(
+                      "images/apple.jpg",
+                      "Fresh Apple",
+                      "1kg, Pricing",
+                      "6.00"
+                    ),
+                    app_items_card(
+                      "images/apple.jpg",
+                      "Fresh Apple",
+                      "1kg, Pricing",
+                      "6.00"
+                    ),
+                  ],
+                ),
+              ),
+          
+              //best selling items
+              SizedBox(height: 50,),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text("Best Selling",
+                    style: app_text_style().copyWith(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black
+                    ),),
+                    Text("See all",
+                    style: app_text_style().copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: primaryColor
+                    ),)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 280,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    app_items_card(
+                      "images/banana.jpg",
+                      "Organic Bananas",
+                      "7pcs, Pricing",
+                      "4.99"
+                    ),
+                    app_items_card(
+                      "images/apple.jpg",
+                      "Fresh Apple",
+                      "1kg, Pricing",
+                      "6.00"
+                    ),
+                    app_items_card(
+                      "images/apple.jpg",
+                      "Fresh Apple",
+                      "1kg, Pricing",
+                      "6.00"
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
